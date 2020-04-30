fd8c05f188607ffb1311c12c0baed8098a9b4ef3
mohamed <mohamednasr8687@gmail.com>
2020-04-29 23:06:19 -0400

    done

---
+++ app.js
@@ -0,0 +1,65 @@
+function computerPlay() {
+  let num = Math.floor(Math.random() * 3 + 1);
+  let choice;
+  if (num === 1) {
+    choice = "rock";
+  } else if (num === 2) {
+    choice = "paper";
+  } else {
+    choice = "scissor";
+  }
+  return choice;
+}
+
+const playerPlay = () => {
+  return prompt("rock, paper, scissor").toLowerCase();
+};
+function playRound(playerSelection, computerSelection) {
+  let result;
+  if (playerSelection === computerSelection) {
+    result = "tie";
+  } else if (
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "rock" && computerSelection === "scissor") ||
+    (playerSelection === "scissor" && computerSelection === "paper")
+  ) {
+    result = "win";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "scissor" && computerSelection === "rock") ||
+    (playerSelection === "paper" && computerSelection === "scissor")
+  ) {
+    result = "lose";
+  }
+  return result;
+}
+
+function game() {
+  let wins = 0;
+  let lose = 0;
+  let tie = 0;
+
+  for (let i = 0; i < 5; i++) {
+    let round = playRound(playerPlay(), computerPlay());
+    if (round === "win") {
+      console.log("you win this round");
+      wins += 1;
+    } else if (round === "lose") {
+      console.log("you lose this round");
+      lose += 1;
+    } else {
+      console.log("tie this round");
+      tie += 1;
+    }
+  }
+
+  if (wins > lose) {
+    return `You are the winner!  win:${wins}, lose:${lose} and tie:${tie} `;
+  } else if (lose > wins) {
+    return `You lose!  win:${wins}, lose:${lose} and tie:${tie} `;
+  } else {
+    return `No winner!  win:${wins}, lose:${lose} and tie:${tie} `;
+  }
+}
+
+console.log(game());
